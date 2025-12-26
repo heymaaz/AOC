2877a443aab4cb175e2bac7c22bab0600655e956
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2025-12-26 08:48:43 +0000

    Day4 part 1

--- TwentyFive/DAY4/part1.ts
+++ TwentyFive/DAY4/part1.ts
@@ -4,4 +4,52 @@ if (!Bun.env.INPUT_FILE) {
 const foo = Bun.file(Bun.env.INPUT_FILE);
 
 const lines: string[] = await foo.text().then((data) => data.trim().split("\n"));
-console.log(lines);
+const arr = lines.map((line) => line.split(""));
+if (!arr || !arr[0] || arr.length === 0 || arr[0].length === 0) {
+  throw new Error("Input file is empty or improperly formatted.");
+}
+
+let out = 0;
+for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    if (!row) {
+        throw new Error(`Row ${i} is undefined.`);
+    }
+    for (let j = 0; j < row.length; j++) {
+        const char = row[j];
+        let c = 0;
+        if (char === "@") {
+            if(j+1 < row.length && arr[i][j+1] === "@") {
+                c++;
+            }
+            if(j-1 >= 0 && arr[i][j-1] === "@") {
+                c++;
+            }
+            if(i+1 < arr.length && arr[i+1][j] === "@") {
+                c++;
+            }
+            if(i-1 >= 0 && arr[i-1][j] === "@") {
+                c++;
+            }
+
+            if(i+1 < arr.length && j+1 < row.length && arr[i+1][j+1] === "@") {
+                c++;
+            }
+            if(i-1 >= 0 && j-1 >= 0 && arr[i-1][j-1] === "@") {
+                c++;
+            }
+            if(i+1 < arr.length && j-1 >= 0 && arr[i+1][j-1] === "@") {
+                c++;
+            }
+            if(i-1 >= 0 && j+1 < row.length && arr[i-1][j+1] === "@") {
+                c++;
+            }
+            if(c < 4) {
+                out++;
+            }
+        }
+    }
+}
+
+console.log(arr);
+console.log(out);
--- TwentyFive/DAY4/part2.ts
+++ TwentyFive/DAY4/part2.ts
@@ -1,2 +1,28 @@
-console.log("Hello via Bun!");
-console.log(Bun.env.INPUT_FILE);
+if (!Bun.env.INPUT_FILE) {
+  throw new Error("INPUT_FILE environment variable is not set.");
+}
+const foo = Bun.file(Bun.env.INPUT_FILE);
+
+const lines: string[] = await foo.text().then((data) => data.trim().split("\n"));
+const arr = lines.map((line) => line.split(""));
+if (!arr || !arr[0] || arr.length === 0 || arr[0].length === 0) {
+  throw new Error("Input file is empty or improperly formatted.");
+}
+
+let out = 0;
+for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    if (!row) {
+        throw new Error(`Row ${i} is undefined.`);
+    }
+    for (let j = 0; j < row.length; j++) {
+        const char = row[j];
+        let c = 0;
+        if (char === "@") {
+            out++;
+        }
+    }
+}
+
+console.log(arr);
+console.log(out);
